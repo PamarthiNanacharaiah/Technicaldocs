4dee6a6307174e9edd0bf77a5b3b377591892db1
PamarthiNanacharaiah <79440993+PamarthiNanacharaiah@users.noreply.github.com>
2021-02-23 16:45:09 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git & Documentations'
+copyright = '2021, Nancharaiah'
+author = 'PamarthiNancharaiah'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
